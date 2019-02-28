0bbb7b615de3f8d2a911f3a9f365cfaf1c6cc42f
Adrian <adriandinca2008@yahoo.com>
2019-02-28 14:27:24 +0200

    fix: Prevent error if there is no slider

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,7 @@ import './components/flickr-widget';
 import './components/login-or-register';
 import './components/mobile-menu';
 
-new Slider(document.querySelector(`.${Slider.CSS_CLASSES.SLIDER}`));
+const sliderElement = document.querySelector(`.${Slider.CSS_CLASSES.SLIDER}`);
+if (sliderElement) {
+  new Slider(sliderElement);
+}

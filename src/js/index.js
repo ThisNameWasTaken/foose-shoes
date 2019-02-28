bcd47ae4c8763b1cc55b0cd3516ea90de2036735
Adrian <adriandinca2008@yahoo.com>
2019-02-28 13:35:32 +0200

    feat: Add login & register modal

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,6 @@ import './components/search-field';
 import './components/blog-news-widget';
 import './components/twitter-widget';
 import './components/flickr-widget';
+import './components/login-or-register';
 
 new Slider(document.querySelector(`.${Slider.CSS_CLASSES.SLIDER}`));

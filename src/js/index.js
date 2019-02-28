64c284587f2f63741b32e4d17624737660ba93df
Adrian <adriandinca2008@yahoo.com>
2019-02-28 12:18:36 +0200

    feat: Lazy load flickr images

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,6 @@ import './images';
 import './components/search-field';
 import './components/blog-news-widget';
 import './components/twitter-widget';
+import './components/flickr-widget';
 
 new Slider(document.querySelector(`.${Slider.CSS_CLASSES.SLIDER}`));

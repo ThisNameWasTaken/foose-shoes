0716cedc8c3bb428841a6abcf317ad24f257acf9
Adrian <adriandinca2008@yahoo.com>
2019-02-28 15:26:52 +0200

    feat: Add toggle buttons for list view and grid view

--- src/js/index.js
+++ src/js/index.js
@@ -11,3 +11,33 @@ const sliderElement = document.querySelector(`.${Slider.CSS_CLASSES.SLIDER}`);
 if (sliderElement) {
   new Slider(sliderElement);
 }
+
+(() => {
+  const gridButton = document.querySelector('#display-grid');
+  const listButton = document.querySelector('#display-list');
+
+  if (!gridButton || !listButton) {
+    return;
+  }
+
+  const shoeCards = [...document.querySelectorAll('.shoe-card')];
+  const gridCells = shoeCards.map(shoeCard => shoeCard.parentElement);
+
+  gridButton.addEventListener('click', () => {
+    console.log('click');
+    shoeCards.forEach(shoeCard => shoeCard.classList.remove('shoe-card--list'));
+    gridCells.forEach(gridCell => {
+      gridCell.classList.add('col-md-6');
+      gridCell.classList.add('col-lg-4');
+    });
+  });
+
+  listButton.addEventListener('click', () => {
+    console.log('click');
+    gridCells.forEach(gridCell => {
+      gridCell.classList.remove('col-md-6');
+      gridCell.classList.remove('col-lg-4');
+    });
+    shoeCards.forEach(shoeCard => shoeCard.classList.add('shoe-card--list'));
+  });
+})();

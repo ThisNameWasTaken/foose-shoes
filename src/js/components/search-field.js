08f9bf7f772f9f54d6d5361be3e0537cfdcbd65b
Adrian <adriandinca2008@yahoo.com>
2019-02-28 10:10:16 +0200

    feat: Make search field interactive

--- src/js/components/search-field.js
+++ src/js/components/search-field.js
@@ -1,7 +1,10 @@
-(() => {
+(async () => {
   const searchFieldInput = document.querySelector('#search-text-field');
   const searchField = searchFieldInput.closest('.search-text-field');
   const searchFieldButton = searchField.querySelector('.search-text-field__button');
+  const searchFieldResults = searchField.querySelector(
+    '.search-text-field__results'
+  );
 
   searchFieldButton.addEventListener('click', event => {
     event.preventDefault();
@@ -9,9 +12,32 @@
     searchFieldInput.focus();
   });
 
+  searchFieldInput.addEventListener('focus', updateSearchResults);
+
   searchFieldInput.addEventListener('blur', () =>
     searchField.classList.remove('search-text-field--active')
   );
+
+  searchFieldInput.addEventListener('keyup', updateSearchResults);
+
+  const searchFieldKeywords = await fetch('/search-field-keywords.json').then(res =>
+    res.json()
+  );
+
+  function updateSearchResults() {
+    searchFieldResults.innerHTML = searchFieldInput.value
+      ? searchFieldKeywords
+          .filter(keyword =>
+            keyword.toLowerCase().includes(searchFieldInput.value.toLowerCase())
+          )
+          .slice(0, 5)
+          .map(
+            result =>
+              `<div class="search-text-field__results__result">${result}</div>`
+          )
+          .join('')
+      : '';
+  }
 })();
 
 (() => {
